ab91b374d88fc860ccd0d32817f1cebb759ece60
TxominBisbau <txominmateobisbau@gmail.com>
2026-07-08 19:59:23 +0000

    Ajout servos pince

--- Manette/main.cpp
+++ Manette/main.cpp
@@ -1,4 +1,4 @@
-// Compilation : g++ main.cpp src/motors.cpp src/servo.cpp src/manette.cpp -o picarpro -std=c++17 -llgpio
+// Compilation : g++ main.cpp src/motors.cpp src/pca9685.cpp src/servo.cpp src/manette.cpp -o picarpro -std=c++17 -llgpio
 //               sudo ./picarpro 5005
 
 #include <iostream>
@@ -28,13 +28,23 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
     int port = std::stoi(argv[1]);
+
     Servo servoDirection(CHANNEL_DIRECTION, 500.0f, 2400.0f, 90.0f);
+    Servo servoRotation(CHANNEL_ROTATION, 500.0f, 2400.0f, 90.0f);
+    Servo servoIncline(CHANNEL_INCINE, 500.0f, 2400.0f, 90.0f);
+    Servo servoPosition(CHANNEL_POSITION, 500.0f, 2400.0f, 90.0f);
+    Servo servoOuvert(CHANNEL_OUVERT, 500.0f, 2400.0f, 90.0f);
+
     if (!Manette::init(port)) return EXIT_FAILURE;
 
     while (running) {
         EtatManette etat = Manette::read();
         int spd = Motors::getSpeed();
         int angle_direction = servoDirection.getAngle();
+        int angle_rotation = servoRotation.getAngle();
+        int angle_incline = servoIncline.getAngle();
+        int angle_position = servoPosition.getAngle();
+        int angle_ouvert = servoOuvert.getAngle();
 
         // Moteurs
         if (etat.btn & BTN_START || (etat.yL >= 24000 && etat.yL <= 40000)) {
@@ -68,9 +78,64 @@ int main(int argc, char** argv) {
             servoDirection.setAngle(angle_direction);
         }
         if (etat.xL < 24000) {
-            angle_direction = 120 - etat.xL*6/2400;
+            angle_direction = 150 - etat.xL*6/2400;
             servoDirection.setAngle(angle_direction);
         }
+
+        // Servo de rotation de la pince
+        if (etat.xR >= 24000 && etat.xR <= 40000) {
+            servoRotation.center();
+        }
+        if (etat.xR > 40000) {
+            if (etat.xR >= 64000) {
+                angle_rotation = 30;
+            }
+            else {
+                angle_rotation = 90 - (etat.xR-40000)*6/2400;
+            }
+            servoRotation.setAngle(angle_rotation);
+        }
+        if (etat.xR < 24000) {
+            angle_rotation = 150 - etat.xR*6/2400;
+            servoRotation.setAngle(angle_rotation);
+        }
+
+        // Servo d'inclinaison de la pince
+        if (etat.yR >= 24000  && etat.yR <= 40000) {
+            servoIncline.center();
+        }
+        if (etat.yR > 40000) {
+            if (etat.yR >= 64000) {
+                angle_incline = 180;
+            }
+            else {
+                angle_incline = 90 - (etat.yR+40000)*9/2400;
+            }
+            servoIncline.setAngle(angle_incline);
+        }
+        if (etat.yR < 24000) {
+            angle_incline = 90 - etat.yR*6/2400;
+            servoIncline.setAngle(angle_incline);
+        }
+
+        // Servo de position de la pince
+        if (etat.btn & BTN_R1) {
+            angle_position += 1;
+        }
+        if (etat.btn & BTN_R2) {
+            angle_position += -1;
+        }
+        servoPosition.setAngle(angle_position);
+
+        // Servo d'ouverture de la pince
+        if (etat.btn & BTN_L1) {
+            angle_ouvert += 1;
+        }
+        if (etat.btn & BTN_L2) {
+            angle_ouvert += -1;
+        }
+        servoOuvert.setAngle(angle_ouvert);
+        
 
         if (etat.btn & BTN_SELECT) running = false;
     }
--- Manette/src/servo.h
+++ Manette/src/servo.h
@@ -5,6 +5,10 @@ const int   SERVO_I2C_ADDR    = 0x40;
 const float SERVO_FREQ        = 50.0f;    // Hz
 
 const int CHANNEL_DIRECTION = 0;    // Servo de direction
+const int CHANNEL_ROTATION = 1;     // Servo de rotation de la pince
+const int CHANNEL_INCINE = 2;       // Servo d'inclinaison de la pince
+const int CHANNEL_POSITION = 3;     // Servo de position de la pince
+const int CHANNEL_OUVERT = 4;       // Servo d'ouverture de la pince
 
 class Servo {
 public:
